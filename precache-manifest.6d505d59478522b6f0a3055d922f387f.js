b36d8cde9ef491a82f1466a8d7eb98b01cb65c41
anhlin <anhlin@ucsc.edu>
2019-09-10 02:11:03 -0700

    Updates

---
+++ precache-manifest.6d505d59478522b6f0a3055d922f387f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3ef8b655226cf75176030496b0d4f7",
+    "url": "/css-color-visual/index.html"
+  },
+  {
+    "revision": "d41f9c775fda7e7c9530",
+    "url": "/css-color-visual/static/css/main.61d34166.chunk.css"
+  },
+  {
+    "revision": "94ab7dafd7efd9446961",
+    "url": "/css-color-visual/static/js/2.ebb29977.chunk.js"
+  },
+  {
+    "revision": "d41f9c775fda7e7c9530",
+    "url": "/css-color-visual/static/js/main.1a0aec54.chunk.js"
+  },
+  {
+    "revision": "ffaf3a049a51610ea310",
+    "url": "/css-color-visual/static/js/runtime~main.966d6af1.js"
+  }
+]);
